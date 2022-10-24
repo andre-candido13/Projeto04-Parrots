8d34f3416eb151a23adb5cc5360d680599633ea4
Andre Candido <andre.eng1313@gmail.com>
2022-10-24 15:07:15 -0300

    formatação das letras e tirando identificador de cores das divs.

--- game.js
+++ game.js
@@ -31,7 +31,7 @@ function selecionarCartas() {
   }
 
 
-  for (contador = 0; (answerPlayer/2) > contador; contador++) {
+  for (contador = 0; (answerPlayer / 2) > contador; contador++) {
     let teste = listadeCartas;
     cartasSelecionadas.push(listadeCartas[contador])
     cartasSelecionadas.push(listadeCartas[contador])
@@ -45,7 +45,7 @@ function selecionarCartas() {
 
 
 
-function embaralhar () {
+function embaralhar() {
 
   return Math.random() - 0.5;
 
@@ -58,7 +58,7 @@ function cartasNaTela() {
   ul = document.querySelector(`ul`)
 
 
- 
+
 
   for (let contador = 0; contador < cartasSelecionadas.length; contador++) {
 
@@ -66,12 +66,12 @@ function cartasNaTela() {
       `<ul>
       
       <div id="${cartasSelecionadas[contador]}" onclick="virar (this)" class="front-face face">
-        Frente
+        
         <img src="./Imagens/back.png">
       </div>
 
     <div id="${cartasSelecionadas[contador]}" onclick="virar (this)" class="back-face face" >
-        Verso
+        
         <img src="./Imagens/${cartasSelecionadas[contador]}.gif">
        
       </div>
@@ -79,125 +79,74 @@ function cartasNaTela() {
 
       </ul>`
 
-    
+
 
     ul.innerHTML += item;
 
   }
 }
 
-// let clicadas;
+
 
 let primeiraCarta = "";
 let segundaCarta = "";
 
-let acertou=0;
-let errou=0;
+let contadorAcerto = 0;
+let contadorErro = 0;
 
-function verify () {
+function verify() {
 
-let primeira = primeiraCarta.id
-let segunda = segundaCarta.id
+  let primeira = primeiraCarta.id
+  let segunda = segundaCarta.id
 
-if (primeira === segunda) {
-  primeiraCarta="";
-  segundaCarta="";
-  acertou++
-} 
+  if (primeira === segunda) {
+    primeiraCarta = "";
+    segundaCarta = "";
+    contadorAcerto++
+  }
 
   else {
-  setTimeout(() => {
-    primeiraCarta.parentNode.classList.remove("virada")
-    segundaCarta.parentNode.classList.remove("virada")
-    primeiraCarta=""
-    segundaCarta=""
-    errou++
-  },1000)
+    setTimeout(() => {
+      primeiraCarta.parentNode.classList.remove("virada")
+      segundaCarta.parentNode.classList.remove("virada")
+      primeiraCarta = ""
+      segundaCarta = ""
+      contadorErro++
+    }, 1000)
 
-}
-  
-  if (acertou === answerPlayer/2) {
-    
-    conseguiu() 
-    alert ("oi")
   }
+
+  if (contadorAcerto === answerPlayer / 2) {
+
+
+  }
+  conseguiu()
+  alert("oi")
 
 }
 
 function conseguiu() {
-  let totalTentativas = errou + acertou;
-  
+
 }
 
 
+function virar(clicou) {
 
-
-
-function virar (clicou){
-
-  if (clicou.parentNode.classList.contains("virada")){
-    return }
-
-    if (primeiraCarta ===""){
-      clicou.parentNode.classList.add("virada")
-      primeiraCarta=clicou;
-    
-    } 
-    else if (segundaCarta ===""){
-      clicou.parentNode.classList.add("virada")
-      segundaCarta = clicou;
-      verify()
-    }
+  if (clicou.parentNode.classList.contains("virada")) {
+    return
   }
 
+  if (primeiraCarta === "") {
+    clicou.parentNode.classList.add("virada")
+    primeiraCarta = clicou;
 
-
-    
-  //   setTimeout(desvirar, 2000)
-
-  // }
-  
-
-// function desvirar () {
-
-//   let clicadas = document.querySelectorAll(".virada") 
-//   for (let i=0; i < clicadas.length; i++) {
-//     clicadas[i].classList.remove("virada")
-//   }
-
-// }
-
-
-// function virar (clicou){
-
-//   clicou.classList.toggle("virada")
-  
-//   count++;
-
-//   if (count == 2) {
-//     console.log(count)
-    
-
-//     count=0;
-
-    
-//     setTimeout(desvirar, 2000)
-
-//   }
-  
-// }
-
-// function desvirar () {
-
-//   let clicadas = document.querySelectorAll(".virada") 
-//   for (let i=0; i < clicadas.length; i++) {
-//     clicadas[i].classList.remove("virada")
-//   }
-
-// }
-
-
-
+  }
+  else if (segundaCarta === "") {
+    clicou.parentNode.classList.add("virada")
+    segundaCarta = clicou;
+    verify()
+  }
+}
 
 
 selecionarCartas();
